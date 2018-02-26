b5139683c353c1efce7c6b51f6197db9ba42096a
cuiyingbo <245716954@qq.com>
2018-02-26 16:40:02 +0800

    Initial Commit

---
+++ CYBOCDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CYBOCDemo
+//
+//  Created by admin on 2018/2/26.
+//  Copyright © 2018年 zhiyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
